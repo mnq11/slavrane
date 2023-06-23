b9a3ca53db4f446d67028560a94d0eaa4782ce9e
MohammedQalisi <57759177+mnq11@users.noreply.github.com>
2023-06-23 04:26:12 +0200

    Initialize project using Create React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,26 @@
-// App.tsx
-import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { createTheme, ThemeProvider } from '@material-ui/core';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { AppStateProvider } from './AppStateContext';
-import NavigationBar from "./components/Navigation/NavigationBar";
-import { useDarkMode } from './hooks/useDarkMode';
-
-const Dashboard = lazy(() => import('./components/Dashboard/Dashboard'));
-const Login = lazy(() => import('./components/Login/Login'));
-const Register = lazy(() => import('./components/Register/Register'));
-const Home = lazy(() => import('./components/Home/Home'));
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    const [darkMode, setDarkMode] = useDarkMode();
-
-    const theme = React.useMemo(
-        () =>
-            createTheme({
-                palette: {
-                    type: darkMode ? 'dark' : 'light',
-                },
-            }),
-        [darkMode],
-    );
-
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline/>
-            <AppStateProvider>
-                <Router>
-                    <NavigationBar darkMode={darkMode} setDarkMode={setDarkMode}/>
-                    <ToastContainer/>
-                    <div style={{ width: '100%', height: '100%', paddingTop: '64px' }}>
-                        <Suspense fallback={<div>Loading...</div>}>
-                            <Routes>
-                                <Route path="/login" element={<Login/>}/>
-                                <Route path="/register" element={<Register/>}/>
-                                <Route path="/dashboard" element={<Dashboard/>}/>
-                                <Route path="/" element={<Home/>}/>
-                            </Routes>
-                        </Suspense>
-                    </div>
-                </Router>
-            </AppStateProvider>
-        </ThemeProvider>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
